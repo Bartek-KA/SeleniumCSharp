2e25bfd4643068d6845f30bc2eb7615b4c009430
Bartlomiej Kazior <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-06-17 21:13:59 +0200

    SecondLesson

--- SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharpTutorials/TestClass.cs
@@ -7,21 +7,46 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using SeleniumCSharpTutorials.BaseClass;
 
 namespace SeleniumCSharpTutorials
 {
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                return wait.Until(drv => drv.FindElement(by));
+            }
+            return driver.FindElement(by);
+        }
+    }
     [TestFixture]
     public class TestClass : BaseTest 
     {
         [Test]
+        [Category("Smoke Testing")]
         public void TestMethod1()
         {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium CSharp");
+            driver.FindElement(By.XPath(".//*[@class='_42ft _4jy0 _9xo6 _4jy3 _4jy1 selected _51sy']")).Click();
+            IWebElement createAccountBtn = driver.FindElement(By.XPath(".//*[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']"));
+            createAccountBtn.Click();
+            Thread.Sleep(2000);
+            IWebElement monthDropdownList = driver.FindElement(By.XPath(".//*[@class='_9407 _5dba _9hk6 _8esg']"));
+            SelectElement element = new SelectElement(monthDropdownList);
+            element.SelectByIndex(1);
+            element.SelectByText("Mar");
+            element.SelectByValue("6");          
         }
 
         [Test]
+        [Category("Regression Testing")]
         public void TestMethod2()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
@@ -29,6 +54,7 @@ namespace SeleniumCSharpTutorials
         }
 
         [Test]
+        [Category("Smoke Testing")]
         public void TestMethod3()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
